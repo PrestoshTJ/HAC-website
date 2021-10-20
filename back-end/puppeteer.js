0b11a82ec4938d631a09db9491901cf20fdcd9d1
PrestoshTJ <83054816+PrestoshTJ@users.noreply.github.com>
2021-10-20 17:19:04 -0400

    Update and rename script.js to puppeteer.js

--- back-end/puppeteer.js
+++ back-end/puppeteer.js
@@ -1,20 +1,32 @@
 import puppeteer from 'puppeteer';
 
 const URL = "https://esp41pehac.eschoolplus.powerschool.com/HomeAccess/Account/LogOn"
-// Eventually, this will be customized for each user but for now this website is strictly back-end
-// const username = * Type in username here *
-// const pass = * Type in password here *
+// These variables will be determined by the end user soon
+// const username = Username
+// const pass = Password
+// const classes = [Array with class names]
 
 async function task () {
     const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
-    await page.goto(URL)
-
+    await page.goto(URL, {waitUntil: 'networkidle2'})
     await page.select('.input-validation-error', '350')
     await page.type('#LogOnDetails_UserName', username)
     await page.type('#LogOnDetails_Password', pass)
     await page.click('#login')
-
+    return page
 }
 
-task()
+async function grade (page) {
+    await page.waitForTimeout(2000)
+    await page.click('#hac-Classes')
+}
+
+
+
+async function output () {
+    const page = await task()
+    await grade(page)
+}
+
+output()
